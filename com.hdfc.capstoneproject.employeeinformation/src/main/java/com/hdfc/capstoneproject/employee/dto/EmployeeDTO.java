ab2ae5b4f6b7da11249676bc0bfa20ef8e91d63e
hdfc-repo-adars <adarshverma8u@gmail.com>
2023-04-21 17:06:10 +0530

    uploding project structure and sql file

---
+++ com.hdfc.capstoneproject.employeeinformation/src/main/java/com/hdfc/capstoneproject/employee/dto/EmployeeDTO.java
@@ -0,0 +1,19 @@
+package com.hdfc.capstoneproject.employee.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class EmployeeDTO {
+
+	private long employeeId;
+	private String employeeName;
+	private LocalDate dateOfBirth;
+
+}
