f24ea9890249435472978b45b4dbae7be9a5e973
hdfc-repo-adars <adarshverma8u@gmail.com>
2023-04-22 22:13:32 +0530

    jenkins,log files

--- com.hdfc.capstoneproject.employeeinformation/src/main/java/com/hdfc/capstoneproject/employee/controller/EmployeeRestController.java
+++ com.hdfc.capstoneproject.employeeinformation/src/main/java/com/hdfc/capstoneproject/employee/controller/EmployeeRestController.java
@@ -1,5 +1,9 @@
 package com.hdfc.capstoneproject.employee.controller;
 
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +18,13 @@ import com.hdfc.capstoneproject.employee.service.IEmployeeService;
 @RequestMapping("/capstone/project")
 public class EmployeeRestController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeRestController.class);
 	@Autowired
 	IEmployeeService service;
 	
 	@GetMapping("/get/employee/{employeeId}")
 	public EmployeeDTO getEmployeeById(@PathVariable long employeeId) throws EmployeeNotFoundException {
+		LOGGER.info("Web service called by getEmployeeById at {}", LocalDateTime.now());
 		return service.getEmployeeById(employeeId);
 	}
 }
